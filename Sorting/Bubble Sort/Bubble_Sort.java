d5b8d74255f269845b7c289e3cf3d88ef372fd5c
Muhammad Suleman <145265998+SulemanWaraich@users.noreply.github.com>
2024-05-22 02:03:33 +0500

    Add files via upload

---
+++ Sorting/Bubble Sort/Bubble_Sort.java
@@ -0,0 +1,48 @@
+package Sorting;
+
+public class Bubble_Sort {
+
+    // For Printing The Array
+//    public static void printArr(int arr[])
+//    {
+//        for(int i=0; i< arr.length; i++)
+//        {
+//            System.out.print(arr[i] + " ");
+//        }
+//    }
+
+    public static void main(String[] args){
+
+        int[] arr = {7,8,3,1,2};
+
+        // For Ascending Order
+//        for(int i=0; i<arr.length-1; i++)
+//        {
+//            for(int j=0; j<arr.length-i-1; j++)
+//            {
+//                if(arr[j] > arr[j+1]) {
+//                    int temp;
+//                    temp = arr[j];
+//                    arr[j] = arr[j+1];
+//                    arr[j + 1] = temp;
+//                }
+//            }
+//        }
+
+        // For Descending Order
+//        for(int i=0; i<arr.length-1; i++)
+//        {
+//            for(int j=0; j<arr.length-i-1; j++)
+//            {
+//                if(arr[j] < arr[j+1]) {
+//                    int temp;
+//                    temp = arr[j];
+//                    arr[j] = arr[j+1];
+//                    arr[j + 1] = temp;
+//                }
+//            }
+//        }
+
+//        printArr(arr);
+    }
+}
